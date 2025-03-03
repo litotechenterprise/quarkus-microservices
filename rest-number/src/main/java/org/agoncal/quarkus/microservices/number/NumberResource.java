4b25f93d187788151358a80b6ec3fe4b1fb63852
Pablo Endara-Santiago <litotechenterprise@gmail.com>
2025-03-02 21:32:15 -0500

    completed addeding documentation to the rest endpoint

--- rest-number/src/main/java/org/agoncal/quarkus/microservices/number/NumberResource.java
+++ rest-number/src/main/java/org/agoncal/quarkus/microservices/number/NumberResource.java
@@ -1,16 +1,37 @@
 package org.agoncal.quarkus.microservices.number;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
+
+import java.time.Instant;
+import java.util.Random;
 
 @Path("/api/numbers")
+@Tag(name = "Number Rest Endpoint")
 public class NumberResource {
 
+    @Inject
+    Logger logger;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            summary="Generate book number",
+            description="ISBN 13 and 10 numbers"
+    )
+    public IsbnNumbers hello() {
+        IsbnNumbers isbnNumbers = new IsbnNumbers();
+        isbnNumbers.isbn13 = "13-"+ new Random().nextInt(100_000_000);
+        isbnNumbers.isbn10 = "13-"+ new Random().nextInt(100_000);
+        isbnNumbers.generatedDate = Instant.now();
+
+        logger.info("Generating ISBN number...");
+        return isbnNumbers;
     }
 }
